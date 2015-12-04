a7334f13b7574c61b23dae338478a760ff89b5bd
David Wynn <davidbwynn@gmail.com>
2015-12-04 10:48:39 -0800

    Randomize the post interval to Sumo

--- loggenrunner/loggenrunner.go
+++ loggenrunner/loggenrunner.go
@@ -2,6 +2,7 @@ package loggenrunner
 
 import (
 	"bytes"
+	"math/rand"
 	"net/http"
 	"time"
 
@@ -16,16 +17,28 @@ type DataStore struct {
 // RunLogLine makes repeated calls to an endpoint given the configs of the log line
 func RunLogLine(httpLoc string, postBody string, interval int) {
 	log.Info("Starting log runner")
+
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	// Store the post body... will probably need to radnomize later
 	var tester = []byte(postBody)
+
+	// Begin loop to post the value until we're done
 	for {
-		resp, err5 := http.Post(httpLoc, "text/plain", bytes.NewBuffer(tester))
-		if err5 != nil {
+		// Post to Sumo
+		resp, err := http.Post(httpLoc, "text/plain", bytes.NewBuffer(tester))
+		if err != nil {
 			log.Error("something went amiss on submitting to Sumo")
 			return
 		}
 		defer resp.Body.Close()
 		log.Debug("Response from Sumo: ", resp)
-		time.Sleep(time.Duration(interval) * time.Second)
+
+		// Sleep until the next run
+		// Rnadomize the sleep by specifying the std dev and adding the desired mean... targeting 3%
+		milliseconds := interval * 1000
+		nextInterval := int(r.NormFloat64()*2000.0 + float64(milliseconds))
+		time.Sleep(time.Duration(nextInterval) * time.Millisecond)
 	}
 
 }
