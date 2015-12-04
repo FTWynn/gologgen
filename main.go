a2ead40d5e5dd4abf9edb4f156724ee1b9f77921
David Wynn <davidbwynn@gmail.com>
2015-12-03 21:06:26 -0800

    Working HTTP Post
    That was a very frustrating period of time… the big challenge was
    figuring out how to get in the test text in the HTTP post, because I
    couldn’t find a valid data type.

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	// Read in the config file
+	confText, err := ioutil.ReadFile("gologgen.conf")
+	if err != nil {
+		return
+	}
+
+	// Unmarshal the JSON into a map
+	var cd map[string]string
+	err2 := json.Unmarshal(confText, &cd)
+	if err2 != nil {
+		return
+	}
+
+	// Test post, please ignore
+	var tester = []byte("Test post, please ignore")
+	//c := bufio.Reader.ReadString(tester)
+	//copy(c, tester)
+	resp, err := http.Post(cd["httpLoc"], "text/plain", bytes.NewBuffer(tester))
+	defer resp.Body.Close()
+	fmt.Println(resp)
 }
