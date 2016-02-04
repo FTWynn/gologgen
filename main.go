ce79e54162df88a433c2293bc361d80587d381fb
David Wynn <Remixer96@gmail.com>
2016-02-03 22:38:26 -0800

    Add conf file and data file validation

--- main.go
+++ main.go
@@ -175,13 +175,15 @@ func storeDataFileLogLines(confData GlobalConfStore) (logLines []loggensender.Lo
 			continue
 		}
 
+		validateDataFile(&dataJSON)
+
 		// Add the parsed fields to the result value
 		for i := 0; i < len(dataJSON.Lines); i++ {
 			// Bail if no Text
 			if dataJSON.Lines[i].Text == "" {
 				log.WithFields(log.Fields{
 					"json": dataJSON.Lines[i],
-				}).Error("Line in data file must have a Text value")
+				}).Error("Every line in a data file must have a Text value")
 				continue
 			}
 			logLines = append(logLines, dataJSON.Lines[i])
@@ -274,6 +276,147 @@ func storeDataFileLogLines(confData GlobalConfStore) (logLines []loggensender.Lo
 	return
 }
 
+// validateConfFile ONLY does sanity checks on the values inside the conf file.
+// The data file validation is handled elsewhere
+func validateConfFile(confData *GlobalConfStore) {
+
+	// Confirm the OutputType is valid
+	if confData.OutputType != "http" && confData.OutputType != "syslog" && confData.OutputType != "file" {
+		log.WithFields(log.Fields{
+			"OutputType": confData.OutputType,
+		}).Fatal("Output type in global conf is not in (http, syslog, file)")
+	}
+
+	// Confirm the HTTP Location is valid
+	if r, _ := regexp.Compile("^https?://.*"); confData.OutputType == "http" && !(r.MatchString(confData.HTTPLoc)) {
+		log.WithFields(log.Fields{
+			"HTTPLocation": confData.HTTPLoc,
+		}).Fatal("HTTP Location does not start with http:// or https://")
+	}
+
+	//Confirm SyslogLocation is valid
+	if r, _ := regexp.Compile("^.*?:\\d+$"); confData.OutputType == "syslog" && !(r.MatchString(confData.SyslogLoc)) {
+		log.WithFields(log.Fields{
+			"SyslogLocation": confData.SyslogLoc,
+		}).Fatal("Syslog Location is badly formatted according to regex: ^.*?:\\d+$")
+	}
+
+	// Confirm the SyslogType is valid
+	if confData.OutputType == "syslog" && confData.SyslogType != "tcp" && confData.SyslogType != "udp" {
+		log.WithFields(log.Fields{
+			"SyslogType": confData.SyslogType,
+		}).Fatal("Syslog type in global conf is not in (tcp, udp)")
+	}
+
+	// Confirm File Location is valid
+	if confData.OutputType == "file" && confData.FileOutputPath == "" {
+		log.Fatal("The output file path must be present, and non-blank in the global config if using the file output method")
+	}
+
+	// Loop over all the data files, if any are present
+	if len(confData.DataFiles) > 0 {
+		for i := 0; i < len(confData.DataFiles); i++ {
+			// Confirm the path is non-blank
+			if confData.DataFiles[i].Path == "" {
+				log.Fatal("All data files must have a non-blank path in the global config")
+			}
+		}
+	}
+
+	// Loop over all replay files, if any are present
+	if len(confData.ReplayFiles) > 0 {
+		for i := 0; i < len(confData.ReplayFiles); i++ {
+			// Confirm the path is non-blank
+			if confData.ReplayFiles[i].Path == "" {
+				log.Fatal("All replay files must have a non-blank path in the global config")
+			}
+
+			// Confirm the timestamp regex compiles
+			if _, err := regexp.Compile(confData.ReplayFiles[i].TimestampRegex); err != nil {
+				log.WithFields(log.Fields{
+					"regex":     confData.ReplayFiles[i].TimestampRegex,
+					"error_msg": err,
+				}).Fatal("This regex in the conf file is not valid in the Go regex parser")
+			}
+
+			// Confirm the timestamp format is not blank (I couldn't find an err returning time function)
+			if confData.ReplayFiles[i].TimestampFormat == "" {
+				log.Fatal("All replay files must have a TimestampFormat")
+			}
+
+			// Confirm the Repeat Interval is an integer
+			// This should be handled by JSON Marshaling, so I just need to check for zero
+			if confData.ReplayFiles[i].RepeatInterval == 0 {
+				log.WithFields(log.Fields{
+					"repeatInterval": confData.ReplayFiles[i].RepeatInterval,
+				}).Fatal("The repeat interval must be a non-zero integer")
+			}
+
+			// Confirm all the Headers have the needed fields, if any exist
+			if len(confData.ReplayFiles[i].Headers) > 0 {
+				for k := 0; k < len(confData.ReplayFiles[i].Headers); k++ {
+					if confData.ReplayFiles[i].Headers[k].Header == "" || confData.ReplayFiles[i].Headers[k].Value == "" {
+						log.WithFields(log.Fields{
+							"Header": confData.ReplayFiles[i].Headers[k].Header,
+							"Value":  confData.ReplayFiles[i].Headers[k].Value,
+						}).Fatal("Both the header and value need to be non-zero in the replay file definition")
+					}
+				}
+			}
+		}
+	}
+}
+
+// validateDataFile will do a sanity check on all values in a data file,
+// displaying useful errors and aborting if need be
+func validateDataFile(dataJSON *LogGenDataFile) {
+
+	// Loop through all line objects in the file
+	if len(dataJSON.Lines) > 0 {
+		for i := 0; i < len(dataJSON.Lines); i++ {
+
+			//Confirm Text field exists
+			if dataJSON.Lines[i].Text == "" {
+				log.WithFields(log.Fields{
+					"lineJSON": dataJSON.Lines[i],
+				}).Error("Text field cannot be empty string or missing in data file JSON")
+				continue
+			}
+
+			// Confirm IntervalSecs is not zero
+			if dataJSON.Lines[i].IntervalSecs == 0 {
+				log.WithFields(log.Fields{
+					"lineJSON": dataJSON.Lines[i],
+				}).Error("IntervalSecs field cannot be 0 or missing in data file JSON")
+				continue
+			}
+
+			// IntervalStdDev can be zero... so no sanity checks possible here
+
+			//Confirm Timestamp format field exists
+			if dataJSON.Lines[i].TimestampFormat == "" {
+				log.WithFields(log.Fields{
+					"lineJSON": dataJSON.Lines[i],
+				}).Error("TimestampFormat field cannot be empty string or missing in data file JSON")
+				continue
+			}
+
+			// Confirm all the Headers have the needed fields, if any exist
+			if len(dataJSON.Lines[i].Headers) > 0 {
+				for k := 0; k < len(dataJSON.Lines[i].Headers); k++ {
+					if dataJSON.Lines[i].Headers[k].Header == "" || dataJSON.Lines[i].Headers[k].Value == "" {
+						log.WithFields(log.Fields{
+							"lineJSON": dataJSON.Lines[i],
+						}).Fatal("Both the header and value need to be non-zero in the data file JSON")
+					}
+				}
+			}
+
+		}
+	}
+
+}
+
 func main() {
 	fmt.Println("Starting main program")
 
@@ -291,15 +434,13 @@ func main() {
 	if err != nil {
 		log.WithFields(log.Fields{
 			"error_msg": err,
-			"text":      confText,
-		}).Fatal("Something went amiss on parsing the global config file: ")
+			"text":      string(confText),
+		}).Fatal("Something went amiss on parsing the global config file")
 	}
 
 	fmt.Println("Config File Parsed")
 
-	log.WithFields(log.Fields{
-		"confData": confData,
-	}).Debug("Parsed global config results")
+	validateConfFile(&confData)
 
 	// Bail on essential missing configs
 	if confData.OutputType == "" || (len(confData.DataFiles) == 0 && len(confData.ReplayFiles) == 0) {
@@ -337,6 +478,11 @@ func main() {
 	}).Debug("Finished RunTable")
 
 	fmt.Println("LogLines imported")
+
+	// Bail if all lines were skipped for some reason (bad file paths, etc)
+	if len(RunTable) == 0 {
+		log.Fatal("0 Processed LogLines in RunTable, exiting")
+	}
 
 	fmt.Println("==== Starting the main event loop (set the log level to INFO for more detail) ====")
 
