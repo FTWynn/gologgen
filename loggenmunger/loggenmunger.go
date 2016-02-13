1c1f09acfe643bc2c1e0dfcebdee685b42d2c4e8
David Wynn <Remixer96@gmail.com>
2016-02-12 20:20:23 -0800

    Refactor tokenization selection

--- loggenmunger/loggenmunger.go
+++ loggenmunger/loggenmunger.go
@@ -1,6 +1,7 @@
 package loggenmunger
 
 import (
+	"errors"
 	"math/rand"
 	"regexp"
 	"strconv"
@@ -44,73 +45,23 @@ func RandomizeString(text string, timeformat string) string {
 
 	// Create a list of new strings to be inserted where the tokens were
 	var newstrings []string
-	replacer := strings.NewReplacer("$[", "", "]", "")
 
 	// Append the properly randomized values to the newstrings slice
 	for _, rando := range randos {
-		// Take off the leading and trailing formatting
-		tempstring := replacer.Replace(rando)
-		log.WithFields(log.Fields{
-			"tempstring": tempstring,
-		}).Debug("Removing the formatting from the items")
-
-		// Split the randomizer into individual items
-		tempstrings := strings.Split(tempstring, ",")
-		log.WithFields(log.Fields{
-			"tempstrings": tempstrings,
-			"count":       len(tempstrings),
-		}).Debug("Splitting the random tokens up")
-
-		// Numeric ranges will only have two items for an upper and lower bound,
-		// timestamps have "time" and "stamp", all the rest are string groups
-		var randType string
-		num0, err := strconv.Atoi(string(tempstrings[0]))
-		num1, err2 := strconv.Atoi(string(tempstrings[1]))
-		log.WithFields(log.Fields{
-			"num":       num0,
-			"error_msg": err,
-		}).Debug("Parsing entry 0 as a number")
-		log.WithFields(log.Fields{
-			"num":       num1,
-			"error_msg": err2,
-		}).Debug("Parsing entry 1 as a number", "num", num1, "error", err2)
-
-		switch {
-		case len(tempstrings) == 2 && err == nil && err2 == nil:
-			randType = "Number"
-		case tempstrings[0] == "time" && tempstrings[1] == "stamp":
-			randType = "Timestamp"
-		default:
-			randType = "Category"
+		value, err := getOneToken(rando, timeformat)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"error":        err,
+				"randomTokens": rando,
+			}).Warn("Couldn't get value from token list. Using default TOKEN_ERROR")
+			value = "TOKEN_ERROR"
 		}
 
-		log.WithFields(log.Fields{
-			"type": randType,
-		}).Debug("Finished determining token type")
-
-		switch randType {
-		case "Category":
-			newstrings = append(newstrings, tempstrings[rand.Intn(len(tempstrings))])
-		case "Number":
-			// Get a random number in the range
-			diff := num1 - num0
-			log.Debug("Difference from second and first numbers: ", "diff - ", diff)
-			tempnum := rand.Intn(diff)
-			log.Debug("Random number from zero adjusted spread: ", "rand - ", tempnum)
-			log.Debug("Random number adjusted to range and string converted: ", "rand - ", strconv.Itoa(tempnum+num0))
-			newstrings = append(newstrings, strconv.Itoa(tempnum+num0))
-		case "Timestamp":
-			t := time.Now()
-			timeformatted, err := formatTimestamp(t, timeformat)
-			if err != nil {
-				log.WithFields(log.Fields{
-					"error_msg": err,
-				}).Error("Formatting the timestamp broke")
-			}
-			newstrings = append(newstrings, timeformatted)
-		}
+		newstrings = append(newstrings, value)
 	}
 
+	// We can use the same regex that found the tokens to split up the original string
+	// That gives us clear insertion points for our selected tokens
 	nonRandomStrings := re.Split(text, -1)
 	var newLogLine []string
 
@@ -124,6 +75,76 @@ func RandomizeString(text string, timeformat string) string {
 	log.Debug("Randomization complete: ", "newString - ", strings.Join(newLogLine, ""))
 
 	return strings.Join(newLogLine, "")
+}
+
+func getOneToken(tokenString string, timeformat string) (string, error) {
+	replacer := strings.NewReplacer("$[", "", "]", "")
+
+	// Take off the leading and trailing formatting
+	tempstring := replacer.Replace(tokenString)
+	log.WithFields(log.Fields{
+		"tempstring": tempstring,
+	}).Debug("Removing the formatting from the item list")
+
+	// Split the randomizer into individual items
+	itemList := strings.Split(tempstring, ",")
+	log.WithFields(log.Fields{
+		"itemList": itemList,
+		"count":    len(itemList),
+	}).Debug("Splitting the random tokens up")
+
+	// Numeric ranges will only have two items for an upper and lower bound,
+	// timestamps have "time" and "stamp", all the rest are string groups
+	var randType string
+	num0, err := strconv.Atoi(string(itemList[0]))
+	num1, err2 := strconv.Atoi(string(itemList[1]))
+	log.WithFields(log.Fields{
+		"num":       num0,
+		"error_msg": err,
+	}).Debug("Parsing entry 0 as a number")
+	log.WithFields(log.Fields{
+		"num":       num1,
+		"error_msg": err2,
+	}).Debug("Parsing entry 1 as a number")
+
+	// Set the logic for categorization here, so it's not buried in the switch
+	switch {
+	case len(itemList) == 2 && err == nil && err2 == nil:
+		randType = "Number"
+	case itemList[0] == "time" && itemList[1] == "stamp":
+		randType = "Timestamp"
+	default:
+		randType = "Category"
+	}
+
+	log.WithFields(log.Fields{
+		"type": randType,
+	}).Debug("Finished determining token type")
+
+	switch randType {
+	case "Category":
+		return itemList[rand.Intn(len(itemList))], nil
+	case "Number":
+		// Get a random number in the range
+		diff := num1 - num0
+		log.Debug("Difference from second and first numbers: ", "diff - ", diff)
+		tempnum := rand.Intn(diff)
+		log.Debug("Random number from zero adjusted spread: ", "rand - ", tempnum)
+		log.Debug("Random number adjusted to range and string converted: ", "rand - ", strconv.Itoa(tempnum+num0))
+		return strconv.Itoa(tempnum + num0), nil
+	case "Timestamp":
+		t := time.Now()
+		timeformatted, err := formatTimestamp(t, timeformat)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"error_msg": err,
+			}).Error("Formatting the timestamp broke")
+		}
+		return timeformatted, nil
+	}
+
+	// Failure case. Should never happen.
+	return "", errors.New("Unknown error trying to get a token from the list: " + tokenString)
 }
 
 func formatTimestamp(t time.Time, timeformat string) (string, error) {
